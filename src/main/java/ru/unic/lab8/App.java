3d89390d82970cb96cec6604d92f5aa276c10dec
supercep <supercep110@gmail.com>
2018-03-21 22:59:25 +0300

    commit calc

--- src/main/java/ru/unic/lab8/App.java
+++ src/main/java/ru/unic/lab8/App.java
@@ -6,8 +6,38 @@ package ru.unic.lab8;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+
+        if ((null==args) || (args.length<2))
+        {
+            System.out.println("Error \n\n Please enter correct formule like \n\njava -jar calc.jar 2 + 2 \n\n or \n\njava -jar calc.jar 2 - 2\n\n or \n\njava -jar calc.jar 2 x 2\n\n or \n\njava -jar calc.jar 2 / 2\n\n");
+        }
+        else{
+
+            String action = args[1];
+            int first = Integer.parseInt(args[0]), second = Integer.parseInt(args[2]), result;
+
+            switch (action) {
+                case "+":
+                    result = first + second;
+                    System.out.println(first + " " + action + " " + second + " = " + result);
+                    break;
+                case "-":
+                    result = first - second;
+                    System.out.println(first + " " + action + " " + second + " = " + result);
+                    break;
+                case "x":
+                    result = first * second;
+                    System.out.println(first + " " + action + " " + second + " = " + result);
+                    break;
+                case "/":
+                    result = first / second;
+                    System.out.println(first + " " + action + " " + second + " = " + result);
+                    break;
+                default:
+                    System.out.println("Error \n Please enter correct formule like \n java -jar calc.jar 2 + 2 \n\n or \n\n  java -jar calc.jar 2 - 2 \n\n or \n\n java -jar calc.jar 2 x 2\n\n or \n\njava -jar calc.jar 2 / 2");
+            }
+        }
     }
 }
