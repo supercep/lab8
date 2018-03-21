8c6c72da12b6f17f1281881a9c99afc6c9b87df9
supercep <supercep110@gmail.com>
2018-03-21 22:21:00 +0300

    First commit with .gitignore

---
+++ src/main/java/ru/unic/lab8/App.java
@@ -0,0 +1,13 @@
+package ru.unic.lab8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
