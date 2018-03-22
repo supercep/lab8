57e96693733fa343bc91ba0a5b219eafeef7945c
supercep <supercep110@gmail.com>
2018-03-22 11:57:14 +0300

    JavaFxFrom realize

---
+++ src/main/java/ru/unic/controller/MainController.java
@@ -0,0 +1,16 @@
+package ru.unic.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class MainController {
+
+}
--- src/main/java/ru/unic/lab8/App.java
+++ src/main/java/ru/unic/lab8/App.java
@@ -1,42 +1,28 @@
 package ru.unic.lab8;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args ) {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
 
-
-        if ((null==args) || (args.length<2))
-        {
-            System.out.println("Errorr \n\nPlease enter correct formule like \n\njava -jar calc.jar 2 + 2 \n\n or \n\njava -jar calc.jar 2 - 2\n\n or \n\njava -jar calc.jar 2 x 2\n\n or \n\njava -jar calc.jar 2 / 2\n\n");
-        }
-        else{
-            String action = args[1];
-            int first = Integer.parseInt(args[0]), second = Integer.parseInt(args[2]), result;
-
-            switch (action) {
-                case "+":
-                    result = first + second;
-                    System.out.println(first + " " + action + " " + second + " = " + result);
-                    break;
-                case "-":
-                    result = first - second;
-                    System.out.println(first + " " + action + " " + second + " = " + result);
-                    break;
-                case "x":
-                    result = first * second;
-                    System.out.println(first + " " + action + " " + second + " = " + result);
-                    break;
-                case "/":
-                    result = first / second;
-                    System.out.println(first + " " + action + " " + second + " = " + result);
-                    break;
-                default:
-                    System.out.println("Error \n Please enter correct formule like \n java -jar calc.jar 2 + 2 \n\n or \n\n  java -jar calc.jar 2 - 2 \n\n or \n\n java -jar calc.jar 2 x 2\n\n or \n\njava -jar calc.jar 2 / 2");
-            }
-        }
+    @Override
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/hello.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
